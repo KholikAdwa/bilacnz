74c77f32436fafd347e743cd932314b84314f9ea
kholika2 <kholikadwa14@gmail.com>
2022-04-18 05:48:30 +0700

    bilacnz

---
+++ plugins/donasi.js
@@ -0,0 +1,14 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `››╭─〘 *Donasi* 〙
+╭╡
+│┝‷✧ *Pulsa:* 62895610021383
+│╰───···─────
+│⁺◛˖ Ingin Donasi? Chat
+│wa.me/62895610021383?text=kak+mau+donasi
+╰──────────···───╮
+▌│█║▌║▌║║▌║▌║█│▌
+`.trim(), `${wm}`, 'Owner', '.owner')
+
+module.exports = handler
+
+let wm = global.botwm
